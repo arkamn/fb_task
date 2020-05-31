86f8428184c5a6f323f55b46255859c6c00127c2
Skorobogatov Andrei <arkamn@bk.ru>
2020-05-31 22:27:26 +0300

    Hadler in one .ex

--- lib/handler.ex
+++ lib/handler.ex
@@ -10,15 +10,30 @@ defmodule Handler do
   def parser(request) do
     [method, path] =
       request
+      |> String.split("\n")
+      |> List.first()
       |> String.split(" ")
+
+    [_, params] =
+      request
+      |> String.split("\n")
+
+    parsed_params = parse_params(params)
 
     # At the exit we have a new map {method, path, responce, status}
     %{
       method: String.upcase(method),
       path: path,
+      params: parsed_params,
       response: "",
       status: nil
     }
+  end
+
+  def parse_params(params) do
+    params
+    |> String.trim()
+    |> URI.decode_query()
   end
 
   @doc "Route function compare entrance data with clauses and the generate a new map for HTTP responce and status"
@@ -30,8 +45,9 @@ defmodule Handler do
     %{conv | response: "/visited_links", status: 200}
   end
 
+  # links=
   def route(conv, "POST", "/visited_links") do
-    %{conv | response: "OK", status: 200}
+    %{conv | response: "Links was visited", status: 201}
   end
 
   def route(conv, _, path) do
@@ -45,7 +61,6 @@ defmodule Handler do
     """
   end
 
-  @doc "Turns back status code of a request"
   defp status_reason(code) do
     %{
       200 => "OK",
@@ -53,3 +68,9 @@ defmodule Handler do
     }[code]
   end
 end
+
+# GET /path
+# params
+
+# POST /path
+# params
