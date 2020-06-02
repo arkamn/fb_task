8952f477dd4014928a878b4e65658159c4f942f0
Skorobogatov Andrei <arkamn@bk.ru>
2020-06-02 16:38:24 +0300

    Parser module totaly modified v0.2

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,34 +1,49 @@
 defmodule Parser do
-  defstruct method: "",
-            path: "",
-            params: %{},
-            response: "",
-            status: nil
+  defstruct method: "", path: "", version: "", params: %{}, status: nil
 
-  @doc "Parser function accepts at the entrance the string request and ther make transformation to a new map"
-  def parser(request) do
-    [method, path] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
+  def parse_incom_request(request) do
+    [top, par_string] = String.split(request, "\n\n")
+    [req_line | _head_lines] = String.split(top, "\n")
+    [method, path, version] = String.split(req_line, " ")
+    params = par_string
 
-    [_, params] =
-      request
-      |> String.split("\n")
-
-    parsed_params = parse_params(params)
-
-    %Parser{
-      method: String.upcase(method),
-      path: path,
-      params: parsed_params
-    }
+    %Parser{method: method, path: path, version: version, params: params}
+    |> parse_params
   end
 
-  def parse_params(params) do
-    params
-    |> String.trim()
-    |> URI.decode_query()
+  defp parse_params(%Parser{method: "POST"} = conversion) do
+    params = Poison.decode!(conversion.params)
+    %{conversion | params: params}
+  end
+
+  defp parse_params(%Parser{method: "GET"} = conversion) do
+    trim = String.trim(conversion.params)
+    params = URI.decode_query(trim)
+    %{conversion | params: params}
   end
 end
+
+# curl -XGET -H 'Content-Type: application/json' http://localhost:8999/visited_domains -d '?from=1545221231&to=1545217638'
+get = """
+GET /visited_domains HTTP/1.1
+Host: localhost:8999
+User-Agent: curl/7.43.0
+Accept: */*
+Content-Type: application/json
+ontent-Length: 30
+
+?from=1545221231&to=1545217638
+"""
+
+# curl -H 'Content-Type: application/json' -XPOST http://localhost:8999/visited_links -d '{"links": ["https://ya.ru","https://ya.ru?q=123","funbox.ru","https://stackoverflow.com/questions/11828270/how-to-exit-the-vim-editor"]}'
+# curl -X POST -H "Content-Type: application/json" -d @/Users/arkamn/fb_task/tmp/links.json http://localhost:8999/visited_links
+post = """
+POST /visited_links HTTP/1.1
+Host: localhost:8999
+User-Agent: curl/7.43.0
+Accept: */*
+Content-Type: application/json
+Content-Length: 136
+
+{"links": ["https://ya.ru","https://ya.ru?q=123","funbox.ru","https://stackoverflow.com/questions/11828270/how-to-exit-the-vim-editor"]}
+"""
