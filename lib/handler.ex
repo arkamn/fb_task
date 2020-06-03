44cb2a2379fe951ebbf7448c874c63abd9425973
Skorobogatov Andrei <arkamn@bk.ru>
2020-06-03 11:35:02 +0300

    roure() for POST req and Redis connection added

--- lib/handler.ex
+++ lib/handler.ex
@@ -1,38 +1,44 @@
 defmodule Handler do
   import Parser
+  import Exred
+  import Timestamp
 
   def handler(request) do
     request
-    |> parser
+    |> parse_incom_request
     |> route
     |> response
   end
 
   @doc "Route function compare entrance data with clauses and the generate a new map for HTTP responce and status"
 
-  def route(%Parser{method: "GET", path: "/visited_domains"} = conv) do
-    %{conv | response: "/visited_links", status: 200}
+  def route(%Parser{method: "GET", path: "/"} = conv) do
+    %{conv | status: 200, resp_body: "Hello!"}
   end
 
-  # links=
   def route(%Parser{method: "POST", path: "/visited_links"} = conv) do
-    %{conv | response: "Links was visited", status: 201}
+    map = Poison.Parser.parse!(conv.params, %{})
+    links = map["links"]
+    Enum.each(links, fn link -> set(timestamp(), link) end)
+    %{conv | status: 201, resp_body: "OK"}
   end
 
-  def route(%Parser{path: path} = conv) do
-    %{conv | response: "No path: #{path} here!", status: 404}
+  def route(%Parser{method: _, path: path} = conv) do
+    %{conv | status: 404, resp_body: "No path #{path} here!"}
   end
 
   @doc "Prepare to a response"
   def response(conv) do
     """
-    #{conv.response} #{status_reason(conv.status)}
+    #{conv.version} #{conv.resp_body} #{status_reason(conv.status)}
+    #{conv.params}
     """
   end
 
   defp status_reason(code) do
     %{
       200 => "OK",
+      201 => "Created",
       404 => "Not Found"
     }[code]
   end
