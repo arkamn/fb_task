0d5d243fa71b938fe1b72c63cded0ffd46ddf146
Skorobogatov Andrei <arkamn@bk.ru>
2020-06-04 23:49:10 +0300

    Pop and push functions for Redis completed

--- lib/exred.ex
+++ lib/exred.ex
@@ -1,6 +1,11 @@
 defmodule Exred do
-  def set(key, val) do
+  def zadd(key, val) do
     {:ok, client} = Exredis.start_link()
-    client |> Exredis.query(["SET", key, val])
+    client |> Exredis.query(["ZADD", "time", key, val])
+  end
+
+  def zrange_by_score(from, to) do
+    {:ok, client} = Exredis.start_link()
+    client |> Exredis.query(["ZRANGEBYSCORE", "time", from, to])
   end
 end
